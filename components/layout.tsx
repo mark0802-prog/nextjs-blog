2456dffd510a20adbb9c99787ed95fbe8ecb88be
mark0802-prog <hayabusa.deu.horn@live.jp>
2022-03-21 23:35:18 +0900

    typescript対応

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,36 +1,43 @@
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const name = 'Shunichi'
-export const siteTitle = 'Next.js Sample Website'
+const name = "Shunichi";
+export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: Boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
         <meta
-          name='description'
-          content='Learn how to build a personal website using Next.js'
+          name="description"
+          content="Learn how to build a personal website using Next.js"
         />
         <meta
-          property='og:image'
+          property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name='og:title' content={siteTitle} />
-        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
             <Image
               priority
-              src='/images/profile.jpg'
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -68,6 +75,6 @@ export default function Layout({ children, home }) {
           </Link>
         </div>
       )}
-    </div >
-  )
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    id: string;
+    date: string;
+    title: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -14,8 +23,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>自己紹介</p>
         <p>
-          (This is a sample website - you'll be building a site like this on{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
 
@@ -36,15 +45,15 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
 
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      allPostsData,
+    },
+  };
+};
